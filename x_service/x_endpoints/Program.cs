80bd17e669ee51bdf0712779d17942c26925203d
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-07-19 22:40:03 +0200

    Update project

--- x_service/x_endpoints/Program.cs
+++ x_service/x_endpoints/Program.cs
@@ -1,9 +1,30 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Load .env file
 DotNetEnv.Env.Load();
 
 // Add services to the container.
+
+string connectionUri =  System.Environment.GetEnvironmentVariable("MONGO_CONNECTIONSTRING");
+
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+// Set the ServerApi field of the settings object to Stable API version 1
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+
+// Send a ping to confirm a successful connection
+try {
+  var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+  Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (Exception ex) {
+  Console.WriteLine(ex);
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
