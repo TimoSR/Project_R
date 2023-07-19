f78f0cbaf987afcf88ec073c5d73d1f5fcb564da
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-07-19 14:37:15 +0200

    Update Project Structure

--- x_service/x_endpoints/Program.cs
+++ x_service/x_endpoints/Program.cs
@@ -1,11 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Load .env file into configuration
+DotNetEnv.Env.Load();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCorsPolicy", builder =>
+    {
+        builder
+            .AllowAnyOrigin() // or specify the allowed origins
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("MyCorsPolicy");
 
 app.UseAuthorization();
 
