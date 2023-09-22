476822368e6d98b068cddfbe73c17f79fdb803cd
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-09-22 22:51:38 +0200

    Update

--- Tim/Authentication_service/Domain/DomainEvents/ProductCreatedEvent.cs
+++ Tim/Authentication_service/Domain/DomainEvents/ProductCreatedEvent.cs
@@ -1,13 +1,13 @@
-using Domain.DomainEvents._Attributes;
-using Domain.DomainEvents._Interfaces;
-using ProtoBuf;
-
-namespace Domain.DomainEvents;
-
-[ProtoContract]
-[TopicName("ProductCreatedTopic")]
-public class ProductCreatedEvent : IPubEvent
-{
-    [ProtoMember(1)]
-    public string test { get; set; }
-}
+// using Domain.DomainEvents._Attributes;
+// using Domain.DomainEvents._Interfaces;
+// using ProtoBuf;
+//
+// namespace Domain.DomainEvents;
+//
+// [ProtoContract]
+// [TopicName("ProductCreatedTopic")]
+// public class ProductCreatedEvent : IPubEvent
+// {
+//     [ProtoMember(1)]
+//     public string test { get; set; }
+// }
--- Tim/Authentication_service/Domain/DomainEvents/ProductDeletedEvent.cs
+++ Tim/Authentication_service/Domain/DomainEvents/ProductDeletedEvent.cs
@@ -1,13 +1,13 @@
-using Domain.DomainEvents._Attributes;
-using Domain.DomainEvents._Interfaces;
-using ProtoBuf;
-
-namespace Domain.DomainEvents;
-
-[ProtoContract]
-[TopicName("ProductDeletedTopic")]
-public class ProductDeletedEvent : IPubEvent
-{
-    [ProtoMember(1)]
-    public string test { get; set; }
-}
+// using Domain.DomainEvents._Attributes;
+// using Domain.DomainEvents._Interfaces;
+// using ProtoBuf;
+//
+// namespace Domain.DomainEvents;
+//
+// [ProtoContract]
+// [TopicName("ProductDeletedTopic")]
+// public class ProductDeletedEvent : IPubEvent
+// {
+//     [ProtoMember(1)]
+//     public string test { get; set; }
+// }
