c1fe906501a5d03d1a7d8f5b271607f303f1b384
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-11-28 06:06:28 +0100

    Simplifying WebHooks

---
+++ Tim/Authentication_service/Application/Controllers/Webhooks/UserManagerWebhooksController.cs
@@ -0,0 +1,42 @@
+using _CommonLibrary.Patterns.RegistrationHooks.Events._Attributes;
+using Application.AppServices._Interfaces;
+using Application.Controllers.Webhooks._Abstract;
+using Domain.UserManagement.Events;
+using Infrastructure.Persistence._Interfaces;
+using Infrastructure.Swagger;
+using Infrastructure.Swagger.Attributes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers.Webhooks
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [SwaggerDoc("UserManagerWebhook")]
+    [ApiVersion("1.0")]
+    [Authorize]
+    public class UserManagerWebhooksController : BaseWebhookController
+    {
+        private readonly IAuthAppServiceV1 _authAppService;
+        private readonly ILogger<UserManagerWebhooksController> _logger;
+        
+        public UserManagerWebhooksController(
+            IAuthAppServiceV1 authAppService,
+            IEventHandler eventHandler,
+            ILogger<UserManagerWebhooksController> logger)
+            : base(eventHandler)
+        {
+            _authAppService = authAppService;
+            _logger = logger;
+        }
+        
+        [AllowAnonymous]
+        [HttpPost("HandleUserCreatedEvent")]
+        [EventSubscription("Auth-service-UserCreatedTopic")]
+        public async Task<IActionResult> HandleUserCreatedEvent()
+        {
+            var data = await OnEvent<UserCreatedEvent>();
+            return Ok(data);
+        }
+    }
+}
